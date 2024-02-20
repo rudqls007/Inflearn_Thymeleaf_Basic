b5d95f458066c0a241b58e468891f9fc8827b7a4
rudqls007 <rudqls007@gmail.com>
2024-02-20 13:40:56 +0900

    타임 리프 - URL

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -82,6 +82,14 @@ public class BasicController {
     }
 
 
+    @GetMapping("/link")
+    public String link(Model model) {
+        model.addAttribute("param1", "data1");
+        model.addAttribute("param2", "data2");
+        return "basic/link";
+    }
+
+
     @Data
     static class User {
         private String username;
