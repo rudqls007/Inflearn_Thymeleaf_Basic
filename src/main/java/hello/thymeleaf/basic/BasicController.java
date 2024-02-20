41a446b220e236d3c707cfed3df42d37fbbc7693
rudqls007 <rudqls007@gmail.com>
2024-02-20 13:34:03 +0900

    타임 리프 - 유틸리티 객체와 날짜

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -10,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -63,11 +64,21 @@ public class BasicController {
         return "basic/basic-objects";
     }
 
+
+
     @Component("helloBean")
     static class HelloBean {
         public String hello(String data) {
             return "Hello" + data;
         }
+
+
+    }
+
+    @GetMapping("/date")
+    public String data(Model model) {
+        model.addAttribute("localDateTime", LocalDateTime.now());
+        return "basic/date";
     }
 
 
